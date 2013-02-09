8e183e88b3f1ff8e80bff5ea115e67fb0e94122a
lmastel@my.wctc.edu <>
2013-02-09 09:19:07 -0600

    Commit 5

---
+++ src/dip/lab2/student/solution1/TipCalculator.java
@@ -0,0 +1,16 @@
+
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author LPM
+ */
+public interface TipCalculator {
+    public enum ServiceQuality {
+        GOOD, FAIR, POOR
+    }
+    
+    public abstract void setServiceRating(ServiceQuality q);
+    
+    public abstract ServiceQuality getServiceQuality();
+}
